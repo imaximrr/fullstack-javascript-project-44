5fa147088e94767b8b965a07f5a0261c643c953d
Max Ivanov <maksim.ivanov.r@gmail.com>
2024-04-12 11:42:58 +0200

    eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import findName from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
-import findName from "../src/cli.js";
 findName();
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
 
-
 const findName = () => {
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-}
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
 
 export default findName;
